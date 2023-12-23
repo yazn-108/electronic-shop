8a3f29ca161606176cc489735c57646e3ff6e168
Ezzedine2000 <gamesalab22@gmail.com>
2023-12-23 19:25:55 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/electronic-shop/',
-  server: {
-    host: true,
-  }
+  base: '/electronic-shop',
 })
